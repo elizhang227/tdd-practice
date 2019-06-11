bc13eafaff79b8e7028d7ee8df1646563017be86
elizhang227 <zhang.eli@hotmail.com>
2019-06-11 10:36:08 -0400

    added test for checking if user exists in database

--- models/users.js
+++ models/users.js
@@ -1,1 +1,41 @@
 const db = require('./conn.js');
+
+class User {
+    constructor(id, first_name, last_name , email, password) {
+        this.id = id;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.email = email;
+        this.password = password;
+    }
+
+    async createUser() {
+        try {
+            const response = await db.one(`
+            insert into users
+                (first_name, last_name, email, password)
+            values
+                ($1, $2, $3, $4)
+            returning id`, [this.first_name, this.last_name, this.email, this.password]);
+            return response;
+        } catch(err) {
+            return err.message
+        }
+    }
+
+    static async userExists(email) {
+        try {
+            const response = await db.one(`
+            select *
+            from users
+            where email='${email}'
+            `)
+            return response;
+        } catch(err) {
+            return err.message
+        }
+    }
+
+}
+
+module.exports = User;
--- test/tests.js
+++ test/tests.js
@@ -6,16 +6,24 @@ chai.use(chaiAsPromised).should();
 
 const User = require('../models/users');
 
-describe('Users model tests', () => {
+describe('Users model tests', async () => {
     // Test method for adding users to the database
-    it('should be a valid user object', async () => {
-        //method should be an instance method because you are creating a new instance of a User
-        //info you want the user class to have will be the parameters
-        //first parameter will be null because of id schema: id, first_name, last_name, email, password
-        const userInstance = new User(null, 'pepe', 'frog', 'PepeTheFrog@aol.com', 'pw');
-        //you need to call the method on the instance of the user and store the value in a variable
-        const theUser = await userInstance.createUser();
-        //expect the result returned from createUser to return an object
-        expect(theUser).to.be.an('object');
+    // it('should be a valid user object', async () => {
+    //     //method should be an instance method because you are creating a new instance of a User
+    //     //info you want the user class to have will be the parameters
+    //     //first parameter will be null because of id schema: id, first_name, last_name, email, password
+    //     const userInstance = new User(null, 'pepe', 'frog', 'PepeTheFrog@aol.com', 'pw');
+    //     //you need to call the method on the instance of the user and store the value in a variable
+    //     const theUser = await userInstance.createUser();
+    //     //expect the result returned from createUser to return an object
+    //     expect(theUser).to.be.an('object');
+    // });
+
+    // Test method for checking user can be found in database
+    it('can be found in database', async () => {
+        //method should be static method because you are just checking if it exists
+        const validUser = await User.userExists('PepeTheFrog@aol.com');
+        expect(validUser).to.be.an('object'); 
+        //expect(validUser).to.not.be.an('undefined');
     });
 });
